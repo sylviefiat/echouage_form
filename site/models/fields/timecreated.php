757a0a4f3770dcebaf456d867f0c1bf13b51bd74
sylviefiat <sylvie.fiat@ird.fr>
2017-11-27 10:22:00 +1100

    feat(version): update des fichier à la v2.0.7

--- site/models/fields/timecreated.php
+++ site/models/fields/timecreated.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version     2.0.6
+ * @version     2.0.7
  * @package     com_cot_forms
  * @copyright   Copyright (C) 2014. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
