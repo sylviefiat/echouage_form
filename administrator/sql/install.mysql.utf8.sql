daa984fe4be4beb152267470f9837013509078b4
Ezechiel Wahoulo <ezechi.wahoulo96@gmail.com>
2018-10-04 17:48:45 +1100

    feat(table): add fields on table (year & month)

--- administrator/sql/install.mysql.utf8.sql
+++ administrator/sql/install.mysql.utf8.sql
@@ -64,12 +64,3 @@ FOR EACH ROW SET NEW.localisation = GeomFromText( CONCAT('POINT(', NEW.observati
 CREATE TRIGGER `#__trig_cot_admin_update` BEFORE UPDATE ON `#__cot_admin`
 FOR EACH ROW SET NEW.localisation = GeomFromText( CONCAT('POINT(', NEW.observation_longitude, ' ', NEW.observation_latitude, ')' ));
 
-CREATE TRIGGER `#__trig_cot_admin_year` AFTER INSERT ON `#__cot_admin`
-FOR EACH ROW BEGIN
-UPDATE `#__cot_admin` SET `observation_year` = YEAR(NOW()) WHERE `id` = NEW.id
-END;
-
-CREATE TRIGGER `#__trig_cot_admin_month` AFTER INSERT ON `#__cot_admin`
-FOR EACH ROW BEGIN
-UPDATE `#__cot_admin` SET `observation_month` = MONTH(NOW()) WHERE `id` = NEW.id
-END;
