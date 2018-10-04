baaabf30266ced222cf1eb428198756f388ef15a
Ezechiel Wahoulo <ezechi.wahoulo96@gmail.com>
2018-10-05 07:53:14 +1100

    feat(table): drop trigger

--- administrator/sql/install.mysql.utf8.sql
+++ administrator/sql/install.mysql.utf8.sql
@@ -63,8 +63,3 @@ FOR EACH ROW SET NEW.localisation = GeomFromText( CONCAT('POINT(', NEW.observati
 
 CREATE TRIGGER `#__trig_cot_admin_update` BEFORE UPDATE ON `#__cot_admin`
 FOR EACH ROW SET NEW.localisation = GeomFromText( CONCAT('POINT(', NEW.observation_longitude, ' ', NEW.observation_latitude, ')' ));
-
-CREATE TRIGGER `#__trig_cot_admin_year` AFTER INSERT ON `#__cot_admin`
-FOR EACH ROW BEGIN
-UPDATE `#__cot_admin` SET `observation_year` = YEAR(`observation_datetime`) WHERE `id` = NEW.id;
-END;
