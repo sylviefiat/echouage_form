886dc5b5b93b5ebe8637d75f2b67a5b16b14a09c
Ezechiel Wahoulo <ezechi.wahoulo96@gmail.com>
2018-10-16 11:08:50 +1100

    feat(table): trigger change

--- administrator/sql/install.mysql.utf8.sql
+++ administrator/sql/install.mysql.utf8.sql
@@ -62,17 +62,13 @@ PRIMARY KEY (`id`)
 
 
 CREATE TRIGGER `#__trig_stranding_admin_insert` BEFORE INSERT ON `#__stranding_admin`
-FOR EACH ROW SET NEW.localisation = GeomFromText( CONCAT('POINT(', NEW.observation_longitude, ' ', NEW.observation_latitude, ')' ));
+FOR EACH ROW SET NEW.localisation = GeomFromText( CONCAT('POINT(', NEW.observation_longitude, ' ', NEW.observation_latitude, ')' ))
+	     SET NEW.id_location = NEW.id_location+1;
 
 CREATE TRIGGER `#__trig_stranding_admin_update` BEFORE UPDATE ON `#__stranding_admin`
-FOR EACH ROW SET NEW.localisation = GeomFromText( CONCAT('POINT(', NEW.observation_longitude, ' ', NEW.observation_latitude, ')' ));
+FOR EACH ROW SET NEW.localisation = GeomFromText( CONCAT('POINT(', NEW.observation_longitude, ' ', NEW.observation_latitude, ')' ))
+	     SET NEW.id_location = NEW.id_location+1;
 
-CREATE TRIGGER `#__trig_stranding_admin_id_location_insert` AFTER INSERT ON `#__stranding_admin` 
-FOR EACH ROW SET NEW.id_location = NEW.id_location+1;
-
-
-CREATE TRIGGER `#__trig_stranding_admin_id_location_update` AFTER UPDATE ON `#__stranding_admin` 
-FOR EACH ROW SET NEW.id_location = NEW.id_location+1;
 
 
 
