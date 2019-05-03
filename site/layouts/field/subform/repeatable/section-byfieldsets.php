609e9bb48670de431c6685dd5617a0a0ac43dd57
sylvie fiat <sylvie.fiat@ird.fr>
2019-05-03 17:32:35 +1100

    feat(form): added template subform and showon option

--- site/layouts/field/subform/repeatable/section-byfieldsets.php
+++ site/layouts/field/subform/repeatable/section-byfieldsets.php
@@ -18,67 +18,97 @@ defined('_JEXEC') or die;
  * @var array   $buttons    Array of the buttons that will be rendered
  */
 extract($displayData);
-$animal =  $group;
+$sublayout = 'renderfield';
 ?>
-
-
-<div id="suform-<?php echo $group; ?>"
-	class="subform-repeatable-group subform-repeatable-group-<?php echo $unique_subform_id; ?>"
+<div
+	class="subform-repeatable-group subform-repeatable-group-<?php echo $unique_subform_id; ?> <?php echo $basegroup; ?>"
 	data-base-name="<?php echo $basegroup; ?>"
 	data-group="<?php echo $group; ?>"
 >
-<script type="text/javascript">
+<style>
+.fieldset {
+	display:flex;
+	width: 100%;
+	flex-flow:wrap;
+}
+</style>
+<script type="text/javascript">	
 	function displayTab(tabId,id) {
   		event.preventDefault(); 
-  		jQuery("#panel-"+id+" section").each(function(index,value){
-  			if(tabId!==value.id){
-  				jQuery("#panel-"+id+" #"+value.id).hide();
-  			} else {
-  				jQuery("#panel-"+id+" #"+value.id).show();
+  		jQuery(".subform-repeatable-group").each(function(index,value){
+  			if(getID(value)===id){
+  				jQuery(value).find("section").each(function(index,value){
+		  			if(value.className.indexOf(tabId)<0){
+		  				jQuery(value).hide();
+		  			} else {
+		  				jQuery(value).show();
+		  			}
+		  		})
   			}
-  		})
+  		});
+	}
+	function getID(element) {
+		return element.attributes[2].value;
 	}
 </script>
-	<div class="tab">
-		<div>			
-			<button onclick="displayTab('','<?php echo $animal; ?>')"><?php echo $animal; ?></button>
+	<?php if (!empty($buttons)) : ?>
+	<div class="tab btn-toolbar">
+		<div class="">			
+			<button onclick="displayTab('none',getID(this.parentElement.parentElement.parentElement))">Animal</button>
 			<?php foreach ($form->getFieldsets() as $fieldset) : ?>		
 				<?php if (!empty($fieldset->label)) : ?>			
-					<button onclick="displayTab('<?php echo $fieldset->label; ?>-<?php echo $group; ?>','<?php echo $group; ?>')"><?php echo JText::_($fieldset->label); ?></button>
+					<button onclick="displayTab('<?php echo $fieldset->label; ?>',getID(this.parentElement.parentElement.parentElement))"><?php echo JText::_($fieldset->label); ?></button>
 				<?php endif; ?>
 			<?php endforeach; ?>	
 		</div>
-		<div>
-			<?php if (!empty($buttons)) : ?>
 	
-				<?php if (!empty($buttons['add'])) : ?>
-					<a class="btn btn-mini button btn-success group-add group-add-<?php echo $group; ?>" aria-label="<?php echo JText::_('JGLOBAL_FIELD_ADD'); ?>">
-						<span class="fa fa-plus" aria-hidden="true"></span>
-					</a>
-				<?php endif; ?>
-				<?php if (!empty($buttons['remove'])) : ?>
-					<a class="btn btn-mini button btn-danger group-remove group-remove-<?php echo $group; ?>" aria-label="<?php echo JText::_('JGLOBAL_FIELD_REMOVE'); ?>">
-						<span class="fa fa-minus" aria-hidden="true"></span>
-					</a>
-				<?php endif; ?>
-				<?php if (!empty($buttons['move'])) : ?>
-					<a class="btn btn-mini button btn-primary group-move group-move-<?php echo $group; ?>" aria-label="<?php echo JText::_('JGLOBAL_FIELD_MOVE'); ?>">
-						<span class="fa fa-arrows-alt" aria-hidden="true"></span>
-					</a>
-				<?php endif; ?>
+		<div class="btn-group">
+			<?php if (!empty($buttons['add'])) : ?>
+				<a class="btn btn-mini button btn-success group-add group-add-<?php echo $unique_subform_id; ?>" aria-label="<?php echo JText::_('JGLOBAL_FIELD_ADD'); ?>">
+					<span class="fa fa-plus" aria-hidden="true"></span>
+				</a>
+			<?php endif; ?>
+			<?php if (!empty($buttons['remove'])) : ?>
+				<a class="btn btn-mini button btn-danger group-remove group-remove-<?php echo $unique_subform_id; ?>" aria-label="<?php echo JText::_('JGLOBAL_FIELD_REMOVE'); ?>">
+					<span class="fa fa-minus" aria-hidden="true"></span>
+				</a>
+			<?php endif; ?>
+			<?php if (!empty($buttons['move'])) : ?>
+				<a class="btn btn-mini button btn-primary group-move group-move-<?php echo $unique_subform_id; ?>" aria-label="<?php echo JText::_('JGLOBAL_FIELD_MOVE'); ?>">
+					<span class="fa fa-arrows" aria-hidden="true"></span>
+				</a>
 			<?php endif; ?>
 		</div>
 	</div>
-	<div id="panel-<?php echo $group; ?>" class="row-fluid">	
-		<?php foreach ($form->getFieldsets() as $fieldset) : ?>
-			<section id="<?php echo JText::_($fieldset->label); ?>-<?php echo $group; ?>" class="<?php if (!empty($fieldset->class)){ echo $fieldset->class; } ?> hidden">
-				<?php if (!empty($fieldset->label)) : ?>
-					<legend><?php echo JText::_($fieldset->label); ?></legend>
-				<?php endif; ?>
-				<?php foreach ($form->getFieldset($fieldset->name) as $field) : ?>
-					<?php echo $field->renderField(); ?>
-				<?php endforeach; ?>
-			</section>
-		<?php endforeach; ?>
+	<?php endif; ?>
+	<div class="panel row-fluid">
+<?php foreach ($form->getFieldsets() as $fieldset) : ?>
+<section class="fieldset <?php if (!empty($fieldset->class)){ echo $fieldset->class; } ?> <?php if (!empty($fieldset->label)){ echo $fieldset->label; } ?>" style="display:none;">
+	<?php if (!empty($fieldset->label)) : ?>
+	<legend><?php echo JText::_($fieldset->label); ?></legend>
+	<?php endif; ?>
+<?php foreach ($form->getFieldset($fieldset->name) as $field) : ?>
+	<?php 
+		$rel           = $field->getAttribute('showon')? ' data-showon=\'' . json_encode(JFormHelper::parseShowOnConditions($field->getAttribute('showon'), $field->formControl, $field->group)) . '\'':'';
+ 		$showonEnabled = $field->getAttribute('showon')?true:false;
+		 ?>
+	<?php echo $this->sublayout(
+				$sublayout,
+				array(
+					'options' => array(
+						'class' => $field->class,
+						'labelclass' => $field->labelclass,
+						'rel' => $rel,
+						'hiddenLabel' => $field->type==='Hidden',
+						'showonEnabled' => $field->getAttribute('showon')
+ 					),
+					'label' => $field->label,
+					'input' => $field->input
+				)
+			); 
+	?>
+<?php endforeach; ?>
+</section>
+<?php endforeach; ?>
 	</div>
 </div>
